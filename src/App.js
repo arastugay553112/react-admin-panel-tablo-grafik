395b896f4e7d5f168164c1d1465d2c92d21db615
tugay aras <kes.ici@hotmail.com>
2022-12-15 16:31:21 +0300

    Ayarlar butonu oluşturuldu resimler yüklendi

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const activeMenu=false;
       <BrowserRouter>
       <div className="flex relative dark:bg-main-dark-bg">
         <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}} >
-          <TooltipComponent content="Settings" position="Top">
+          <TooltipComponent content="Ayarlar" position="Top">
             <button type='button' className='text-3xl p-3 hover:drop-shadow-xl
             hover:bg-light-gray text-white' style={{background:'blue',borderRadius:'50%'}}>
               <FiSettings/>
