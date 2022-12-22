de2d6b7042ee44600399819044c977a6e94bf9e2
tugay aras <kes.ici@hotmail.com>
2022-12-22 10:36:31 +0300

    components

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React,{useEffect} from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {FiSettings} from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import './App.css';
+
+const App = () => {
+
+const activeMenu=false;
+
+  return (
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}} >
+          <TooltipComponent content="Ayarlar" position="Top">
+            <button type='button' className='text-3xl p-3 hover:drop-shadow-xl
+            hover:bg-light-gray text-white' style={{background:'blue',borderRadius:'50%'}}>
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg 
+          bg-white'>
+            Sidebar
+            </div>
+        ):(
+          <div className='w-0 dark:bg-secondary-dark-bg'>
+            Sidebar w-0
+          </div>
+        )}
+        <div className={
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? ' md:ml-72 '
+          : 'flex-2' }`
+        }> 
+        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+          Navbar
+        </div>
+        </div>
+        <div>
+          {/* Gösterge Paneli*/}
+          <Routes>
+            <Route path='/' element="Eticaret" />
+            <Route path='/eticaret' element="Eticaret" />
+
+          {/*Sayfalar */}
+          <Route path='/siparis' element="Siparis" />
+          <Route path='/calisan' element="Calisan" />
+          <Route path='/musteri' element="Musteri" />
+
+           {/*Apps */}
+          <Route path='/kanban' element="Kanban" />
+          <Route path='/yonetici' element="Yonetici" />
+          <Route path='/takvim' element="Takvim" />
+          <Route path='/renk-secenegi' element="RenkSecenegi" />
+
+            {/*Grafikler */}
+          <Route path='/cizgi' element="Cizgi" />
+          <Route path='/area' element="Area" />
+          <Route path='/bar' element="Bar" />
+          <Route path='/pie' element="Pie" />
+          <Route path='/finansal' element="Finansal" />
+          <Route path='/renk-eslemesi' element="RenkEslemesi" />
+          <Route path='/piramid' element="Piramid" />
+          <Route path='/yigilmis' element="Yigilmis" />
+
+</Routes>
+        </div>
+      </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
